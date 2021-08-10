06a0718e92c71ca958d6fc9bc06a992ac2e9551f
Zeaven <zeaven25.manalastas@gmail.com>
2021-08-10 20:03:58 +0800

    Added GoalZAddEdit and WalletZAddEdit components

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,42 @@
 <template>
   <div class="flex flex-col h-screen" id="app">
-    <Navbar />
-    <AddForm />
+    <Navbar @show-form-based-title="showForm" />
+    <GoalzAddEdit />
+    <WalletzAddEdit />
   </div>
 </template>
 
 <script>
 import AddForm from "./components/AddForm.vue";
 import Navbar from "./components/Navbar.vue";
+import GoalzAddEdit from "./components/goalz/GoalzAddEdit.vue";
+import WalletzAddEdit from "./components/walletz/WalletzAddEdit.vue";
 
 export default {
   name: "App",
+  data: {
+    title: null,
+    isGoalZ: true,
+  },
+  methods: {
+    showForm(value) {
+      this.title = value;
+      console.log(this.title);
+      if (this.title == "GoalZ") {
+        this.isGoalZ = true;
+      } else {
+        this.isGoalZ = false;
+      }
+      return this.title == "GoalZ"
+        ? (this.isGoalZ = true)
+        : (this.isGoalZ = false);
+    },
+  },
   components: {
     AddForm,
     Navbar,
+    GoalzAddEdit,
+    WalletzAddEdit,
   },
 };
 </script>
