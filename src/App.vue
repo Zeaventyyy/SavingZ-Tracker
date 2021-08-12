b6a13d03ee186bd6493967dc15abf39f8bf7d00f
Zeaven <zeaven25.manalastas@gmail.com>
2021-08-12 22:22:41 +0800

    Imported navbar and fixed layout

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,40 @@
 <template>
   <div class="flex flex-col h-screen" id="app">
-    <Navbar @show-form-based-title="showForm" />
-    <GoalzAddEdit />
-    <WalletzAddEdit />
+    <Navbar />
+    <router-view class="h-full" />
   </div>
 </template>
 
 <script>
-import AddForm from "./components/AddForm.vue";
 import Navbar from "./components/Navbar.vue";
-import GoalzAddEdit from "./components/goalz/GoalzAddEdit.vue";
-import WalletzAddEdit from "./components/walletz/WalletzAddEdit.vue";
 
 export default {
   name: "App",
-  data: {
-    title: null,
-    isGoalZ: true,
-  },
-  methods: {
-    showForm(value) {
-      this.title = value;
-      console.log(this.title);
-      if (this.title == "GoalZ") {
-        this.isGoalZ = true;
-      } else {
-        this.isGoalZ = false;
-      }
-      return this.title == "GoalZ"
-        ? (this.isGoalZ = true)
-        : (this.isGoalZ = false);
-    },
-  },
   components: {
-    AddForm,
     Navbar,
-    GoalzAddEdit,
-    WalletzAddEdit,
   },
 };
 </script>
 
 <style>
 #app {
-  font-weight: 400;
-  font-family: Poppins, Helvetica, sans-serif;
-  font-size: 13px !important;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
