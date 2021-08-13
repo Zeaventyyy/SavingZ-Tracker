12ae32f7b3d29f4ade4bb6431f0580924c4d5515
Zeaven <zeaven25.manalastas@gmail.com>
2021-08-13 22:08:47 +0800

    Added WalletzListing

---
+++ src/views/WalletzListing.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="">
+    <walletz-list />
+  </div>
+</template>
+
+<script>
+import WalletzList from "../components/walletz/WalletzList.vue";
+
+export default {
+  name: "WalletZ",
+  components: {
+    WalletzList,
+  },
+};
+</script>
