ef5d1dfd0320180f893cf984322523a2f6cc5cbc
Zeaven <zeaven25.manalastas@gmail.com>
2021-08-13 22:12:12 +0800

    Added routers for Goalz and Walletz listing

--- src/router/index.js
+++ src/router/index.js
@@ -2,17 +2,29 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import GoalZ from "../views/GoalZ.vue";
 import WalletZ from "../views/WalletZ.vue";
+import GoalzListing from "../views/GoalzListing.vue";
+import WalletzListing from "../views/WalletzListing.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/goalz",
+    name: "GoalZList",
+    component: GoalzListing,
+  },
+  {
+    path: "/add-goalz",
     name: "GoalZ",
     component: GoalZ,
   },
   {
     path: "/walletz",
+    name: "WalletZ",
+    component: WalletzListing,
+  },
+  {
+    path: "/add-walletz",
     name: "WalletZ",
     component: WalletZ,
   },
