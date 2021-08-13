c51781c63069c36e77d4518b3314bd40a456f03d
Zeaven <zeaven25.manalastas@gmail.com>
2021-08-13 22:09:07 +0800

    Added GoalzListing

---
+++ src/views/GoalzListing.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="">
+    <goalz-list />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import GoalzList from "../components/goalz/GoalzList.vue";
+
+export default {
+  name: "GoalZList",
+  components: {
+    GoalzList,
+  },
+};
+</script>
