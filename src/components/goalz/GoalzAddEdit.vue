e7b141e2878368085934baabf1f6ebc5674789be
Zeaven <zeaven25.manalastas@gmail.com>
2021-08-15 19:43:48 +0800

    Added creation of Goalz

--- src/components/goalz/GoalzAddEdit.vue
+++ src/components/goalz/GoalzAddEdit.vue
@@ -33,27 +33,47 @@
         </h1>
         <div class="flex flex-col">
           <label for="Name" class="mr-4 self-start">Name:</label>
-          <input type="text" class="rounded mb-4 " name="Name" />
+          <input
+            type="text"
+            class="rounded mb-4 "
+            v-model="form.name"
+            name="Name"
+          />
           <label for="Amount" class="mr-4 self-start">Amount:</label>
-          <input type="number" class="rounded mb-4" name="Amount" />
+          <input
+            type="number"
+            class="rounded mb-4"
+            v-model="form.amountToSave"
+            name="Amount"
+          />
           <label for="wallets" class="self-start">Choose a wallet:</label>
-          <select class="rounded mb-4" name="wallets" id="wallets">
-            <option value="volvo">BPI1</option>
-            <option value="saab">BPI2</option>
+          <select
+            class="rounded mb-4"
+            name="wallets"
+            id="wallets"
+            v-model="form.wallet"
+          >
+            <option
+              v-for="wallet in walletItems"
+              :key="wallet.id"
+              v-bind:value="wallet"
+              >{{ wallet.name }}</option
+            >
           </select>
         </div>
 
         <div class="flex justify-end">
-          <input
-            class="p-2 bg-red-500 hover:bg-red-900 rounded-lg text-white mr-2 text-xs"
-            type="submit"
-            value="Cancel"
-          />
-          <input
+          <router-link
+            to="/goalz"
+            class="p-2 bg-red-500 hover:bg-red-900 rounded-lg text-white text-xs mr-2"
+            >Cancel
+          </router-link>
+          <button
             class="p-2 bg-blue-500 hover:bg-blue-900 rounded-lg text-white text-xs"
-            type="submit"
-            value="Submit"
-          />
+            @click="addGoalz"
+          >
+            Submit
+          </button>
         </div>
       </form>
     </div>
@@ -61,12 +81,67 @@
 </template>
 
 <script>
+import { v4 as uuidv4 } from "uuid";
 export default {
   data() {
     return {
       menutitle: "GoalZ",
       buttontitle: "Back",
+      form: {
+        id: "",
+        name: "",
+        amountToSave: Number,
+        wallet: {},
+      },
+      goalz: [],
+      walletItems: [],
     };
+  },
+  created() {
+    this.getWalletz();
+    this.getGoalz();
+  },
+  methods: {
+    addGoalz() {
+      // ensure they actually typed something
+      if (!(this.form.amountToSave && this.form.wallet && this.form.name)) {
+        console.log("Di nag save");
+        return;
+      }
+
+      this.form.id = uuidv4();
+      console.log(this.form.id);
+      this.goalz.push(this.form);
+      this.saveGoalz();
+      this.form = "";
+      console.log("Nagdagdag");
+    },
+    removeGoalz(x) {
+      this.goalz.splice(x, 1);
+      this.saveGoalz();
+    },
+    saveGoalz() {
+      const parsed = JSON.stringify(this.goalz);
+      localStorage.setItem("goalz", parsed);
+    },
+    getWalletz() {
+      if (localStorage.getItem("wallets")) {
+        try {
+          this.walletItems = JSON.parse(localStorage.getItem("wallets"));
+        } catch (e) {
+          return;
+        }
+      }
+    },
+    getGoalz() {
+      if (localStorage.getItem("goalz")) {
+        try {
+          this.goalz = JSON.parse(localStorage.getItem("goalz"));
+        } catch (e) {
+          // localStorage.removeItem("goalz");
+        }
+      }
+    },
   },
 };
 </script>
