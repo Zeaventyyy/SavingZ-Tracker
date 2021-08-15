998a529565c0a02fcdd3f4068540cebc6e3fd008
Zeaven <zeaven25.manalastas@gmail.com>
2021-08-15 17:39:35 +0800

    Added listing of wallets

--- src/components/walletz/WalletzList.vue
+++ src/components/walletz/WalletzList.vue
@@ -3,7 +3,7 @@
     <div class="flex flex-1 justify-between items-center p-3 pl-24 pr-24">
       <h1 class="text-lg font-bold">{{ menutitle }}</h1>
       <router-link
-        to="/add-walletz"
+        to="/walletz/create"
         class="flex items-center p-2 bg-blue-500 hover:bg-blue-100 hover:text-blue-500 rounded-lg text-white font-semibold"
       >
         <svg
@@ -27,10 +27,53 @@
     </div>
 
     <div class="h-full w-full justify-center items-center flex bg-gray-100">
-      <ul id="example-1">
-        <li v-for="item in items" :key="item.message">
-          {{ item.message }}
-        </li>
+      <ul>
+        <div
+          class="shadow-lg bg-white rounded w-96 p-4 mb-4"
+          v-for="(wallet, n) in wallets"
+          :key="wallet.id"
+        >
+          <div class="flex justify-between">
+            <span class="font-bold">{{ wallet.name }}</span>
+            <div class="flex">
+              <router-link
+                :to="{
+                  path: `/walletz/${wallet.id}/edit`,
+                }"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5"
+                  viewBox="0 0 20 20"
+                  fill="green"
+                >
+                  <path
+                    d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"
+                  />
+                  <path
+                    fill-rule="evenodd"
+                    d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </router-link>
+              <button @click="removeWallet(n)">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5"
+                  viewBox="0 0 20 20"
+                  fill="red"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 000 2h6a1 1 0 100-2H7z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
       </ul>
     </div>
   </div>
@@ -42,8 +85,32 @@ export default {
     return {
       menutitle: "WalletZ",
       buttontitle: "Add New",
-      items: [{ message: "Cash" }, { message: "Cash" }],
+      wallets: [],
+      walletName: "testing lang",
     };
+  },
+  created() {
+    this.getWallets();
+  },
+
+  methods: {
+    removeWallet(x) {
+      this.wallets.splice(x, 1);
+      this.saveWallets();
+    },
+    saveWallets() {
+      const parsed = JSON.stringify(this.wallets);
+      localStorage.setItem("wallets", parsed);
+    },
+    getWallets() {
+      if (localStorage.getItem("wallets")) {
+        try {
+          this.wallets = JSON.parse(localStorage.getItem("wallets"));
+        } catch (e) {
+          localStorage.removeItem("wallets");
+        }
+      }
+    },
   },
 };
 </script>
