e0ef57e778590d09f20a46ec1808f01828cd5282
Zeaven <zeaven25.manalastas@gmail.com>
2021-08-18 18:06:51 +0800

    Redirected to walletz upon submit and fixed layout

--- src/components/walletz/WalletzAddEdit.vue
+++ src/components/walletz/WalletzAddEdit.vue
@@ -1,27 +1,7 @@
 <template>
   <div class="flex flex-col h-full">
-    <div class="flex flex-1 justify-between items-center p-3 pl-24 pr-24">
+    <div class="flex justify-between items-center ml-24 mr-24 h-16">
       <h1 class="text-lg font-bold">{{ menutitle }}</h1>
-      <router-link
-        to="/walletz"
-        class="flex items-center p-2 bg-blue-500 hover:bg-blue-100 hover:text-blue-500 rounded-lg text-white font-semibold"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z"
-            clip-rule="evenodd"
-          />
-        </svg>
-        <div>
-          {{ buttontitle }}
-        </div>
-      </router-link>
     </div>
 
     <div class="h-full w-full justify-center items-center flex bg-gray-100">
@@ -117,6 +97,7 @@ export default {
     saveWallets() {
       const parsed = JSON.stringify(this.wallets);
       localStorage.setItem("wallets", parsed);
+      this.$router.push({ path: "/walletz" });
     },
     formData() {
       if (this.destination != null) {
