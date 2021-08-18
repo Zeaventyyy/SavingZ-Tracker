93bb36bff635d52604d72c794fbdad1b22c8463c
Zeaven <zeaven25.manalastas@gmail.com>
2021-08-18 18:02:17 +0800

    Imported sweetalert2

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "./index.css";
-import router from './router'
+import router from "./router";
+import VueSweetalert2 from "vue-sweetalert2";
+import "sweetalert2/dist/sweetalert2.min.css";
 
 Vue.config.productionTip = false;
 
+Vue.use(VueSweetalert2);
 new Vue({
   el: "#app",
   router,
-  render: (h) => h(App)
+  render: (h) => h(App),
 });
