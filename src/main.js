5c0a3e758588955e1edc410ea88a6ecece7f8896
Zeaven <zeaven25.manalastas@gmail.com>
2021-09-06 18:07:41 +0800

    Added toast notification

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import router from "./router";
 import VueSweetalert2 from "vue-sweetalert2";
 import "sweetalert2/dist/sweetalert2.min.css";
 import "./registerServiceWorker";
+import VueToast from "vue-toast-notification";
+// Import one of the available themes
+//import 'vue-toast-notification/dist/theme-default.css';
+import "vue-toast-notification/dist/theme-sugar.css";
+
+Vue.use(VueToast);
 
 Vue.config.productionTip = false;
 
