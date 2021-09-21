49ef2fb4796fe02a122eb09ce925a723eee09d2f
Zeaven <zeaven25.manalastas@gmail.com>
2021-09-21 16:39:44 +0800

    Imported goalz edit component to goalz edit router

---
+++ src/views/goalz/GoalzEdit.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="">
+    <goalz-edit />
+  </div>
+</template>
+
+<script>
+import GoalzEdit from "../../components/goalz/GoalzEdit.vue";
+
+export default {
+  name: "GoalZ",
+  components: {
+    GoalzEdit,
+  },
+};
+</script>
